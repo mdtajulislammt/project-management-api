1f8403d1aeabbf03259a1becf6b4b22c9b3db441
MD Tajul Islam <mdtajulislm50599@gmail.com>
2025-07-21 21:15:33 +0600

    now application is ready for build

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,6 @@ async function bootstrap() {
 
   const reflector = app.get(Reflector);
 
-  // 🔥 Apply JwtAuthGuard first, then RolesGuard
-  // app.useGlobalGuards(new JwtAuthGuard(), new RolesGuard(reflector));
-  // Now only use RolesGuard
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
